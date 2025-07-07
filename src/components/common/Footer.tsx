65ad321b5df4af7c95bec9dca9b155510129309d
Enzo Ibarra <enz0ibarra.cai@gmail.com>
2025-07-07 10:17:30 -0300

    footer realizado

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -68,8 +68,8 @@ export default function Footer() {
 
 			<div className='max-w-7xl mx-auto px-6 py-4 text-center text-sm space-y-1'>
 				<p>
-					Proyecto final creado por estudiantes de
-					<p className='font-semibold'>SoyHenry</p>
+					Proyecto final creado por estudiantes de{' '}
+					{<span className='font-semibold'>SoyHenry</span>}
 				</p>
 				<p>© 2025 Héroes Cercanos. Todos los derechos reservados.</p>
 			</div>
